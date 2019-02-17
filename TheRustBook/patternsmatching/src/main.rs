358d60e3792bfe21950dab7864e18aac87e3bdf2
devedge <richard.pjski2@gmail.com>
2019-02-17 18:10:36 -0500

    Destructure enums

--- TheRustBook/patternsmatching/src/main.rs
+++ TheRustBook/patternsmatching/src/main.rs
@@ -1,12 +1,21 @@
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
-    let x = Some(5);
-    let y = 10;
+    let msg = Message::ChangeColor(0, 160, 255);
 
-    match x {
-        Some(50) => println!("Got 50"),
-        Some(y) => println!("Matched, y = {:?}", y),
-        _ => println!("Default case, x = {:?}", x),
+    match msg {
+        Message::Quit => println!("The Quit variant has no data to destructure"),
+        Message::Move { x, y } => {
+            println!("Move in the x direction {} and in the y direction {}", x, y);
+        }
+        Message::Write(text) => println!("Text message: {}", text),
+        Message::ChangeColor(r, g, b) => {
+            println!("Change the color to red {}, green {}, and blue {}", r, g, b)
+        }
     }
-
-    println!("at the end: x = {:?}, y = {:?}", x, y);
 }
