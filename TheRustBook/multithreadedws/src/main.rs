a65f421f529502844538e917b73035813c6502d9
devedge <richard.pjski2@gmail.com>
2019-02-18 01:06:14 -0500

    Synchronous finalized

--- TheRustBook/multithreadedws/src/main.rs
+++ TheRustBook/multithreadedws/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -14,10 +15,18 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "../hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "../404.html")
+    }
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
