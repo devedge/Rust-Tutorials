0b5e398537cd018d150469f7d8fcb95c3b8a9485
devedge <richard.pjski2@gmail.com>
2019-02-23 21:21:23 -0500

    Terminate treads, and finished multithreaded WS project

--- TheRustBook/multithreadedws/src/bin/main.rs
+++ TheRustBook/multithreadedws/src/bin/main.rs
@@ -3,19 +3,20 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-// use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
         pool.execute(|| {
             handle_connection(stream);
         });
     }
+
+    println!("Shutting down.");
 }
 
 fn handle_connection(mut stream: TcpStream) {
@@ -25,9 +26,9 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "../hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "../404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
